8afb4089d5e98c0f890c20498e8bc321cbb88a70
Sefat <dsisefat@gmail.com>
2023-10-12 14:14:26 +0600

    All Tab

---
+++ All Important Tab link.bat
@@ -0,0 +1,5 @@
+Start https://dshe.portal.gov.bd/
+Start https://www.facebook.com/amc.finance/
+Start https://www.dsebd.org/index.php
+Start https://www.nu.ac.bd/recent-news-notice.php
+Start https://mail.google.com/mail/u/0/?hl=en#inbox
