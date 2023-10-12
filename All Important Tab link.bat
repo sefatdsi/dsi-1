f2c180cc474bf4cd1cf054f5fa4253e409a2e175
Sefat <dsisefat@gmail.com>
2023-10-12 14:27:14 +0600

    Delete All Important Tab link.bat

--- All Important Tab link.bat
+++
@@ -1,5 +0,0 @@
-Start https://dshe.portal.gov.bd/
-Start https://www.facebook.com/amc.finance/
-Start https://www.dsebd.org/index.php
-Start https://www.nu.ac.bd/recent-news-notice.php
-Start https://mail.google.com/mail/u/0/?hl=en#inbox
